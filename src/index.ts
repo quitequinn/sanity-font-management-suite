f2bab5f85400c69abacfbafda2f085886a92e19c
Quinn Keaveney <quinn@liiift.studio>
2025-08-13 23:23:42 -0700

    Add main index file with exports

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+export { FontUploaderComponent } from './FontUploaderComponent'
+export { GenerateCollectionsPairsComponent } from './GenerateCollectionsPairsComponent'
+export { useSanityClient } from './hooks/useSanityClient'
+
+// Utility functions
+export { generateCssFile } from './utils/generateCssFile'
+export { generateFontData } from './utils/generateFontData'
+export { generateFontFile } from './utils/generateFontFile'
+
+// Re-export default
+export { default } from './FontUploaderComponent'
